0d4200bca821a3f2ddbf55b29916d866a71faa31
Gabe Colon <gabriel@letca.com>
2019-03-29 20:34:44 -0400

    Second query

--- logs.py
+++ logs.py
@@ -9,3 +9,11 @@ JOIN log
 ON articles.slug = substring(log.path, 10)
 GROUP BY title ORDER BY views DESC limit 3;
 )
+
+request2 = "Who are the most popular authors of all time?"
+
+query2 = (SELECT authors.name, count(*) as views FROM log , articles, authors
+WHERE articles.slug = substring(log.path, 10)
+AND articles.author = authors.id
+GROUP BY authors.name
+ORDER BY views DESC;)
