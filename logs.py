1b55f5f1f921dec5c1e3efd83325d60965ab88af
Gabe Colon <gabriel@letca.com>
2019-03-29 20:47:16 -0400

    typo on line 7

--- logs.py
+++ logs.py
@@ -4,7 +4,7 @@ DBNAME = "news"
 
 request1 = "What are the most popular articles of all time?"
 
-query1 = ( SELECT title, count(*) as views FROM articles
+query1 = ( SELECT title , count(*) as views FROM articles
 JOIN log
 ON articles.slug = substring(log.path, 10)
 GROUP BY title ORDER BY views DESC limit 3;
